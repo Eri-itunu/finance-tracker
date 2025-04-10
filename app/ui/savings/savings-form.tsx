d4ef0e47b3223673cd7db5909fc893f701c4d4c0
eriitunu-a <adesioyeeriitunu@gmail.com>
2025-04-10 20:54:41 +0100

    Update savings-form.tsx

--- app/ui/savings/savings-form.tsx
+++ app/ui/savings/savings-form.tsx
@@ -50,7 +50,7 @@ export default function Create({
               <select
                 id="savingsId"
                 name="savingsId"
-                className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+                className="peer  block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
                 defaultValue=""
                 aria-describedby="savingsgoal-error"
               >
@@ -63,7 +63,7 @@ export default function Create({
                   </option>
                 ))}
               </select>
-              <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
+              <UserCircleIcon className="hidden md-block pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
             </div>
             <div id="category-error" aria-live="polite" aria-atomic="true">
               {state.errors?.savingsId &&
