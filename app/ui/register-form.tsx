79834d208c2cfab7c7edcebf0793cd0f9de6bd39
eriitunu-a <adesioyeeriitunu@gmail.com>
2025-04-16 14:04:35 +0100

    changing text colour to green

--- app/ui/register-form.tsx
+++ app/ui/register-form.tsx
@@ -148,7 +148,7 @@ export default function RegisterForm() {
             <KeyIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
           </div>
           {errorMessage?.errors?.confirmPassword && (
-            <p className="text-sm text-red-500">
+            <p className="text-sm text-green-500">
               {errorMessage.errors.confirmPassword[0]}
             </p>
           )}
@@ -169,7 +169,7 @@ export default function RegisterForm() {
           {errorMessage?.message && (
             <>
               <ExclamationCircleIcon className="h-5 w-5 text-red-500" />
-              <p className="text-sm text-red-500">{errorMessage.message}</p>
+              <p className="text-sm text-green-500">{errorMessage.message}</p>
             </>
           )}
         </div>
--- app/ui/savings/goals-form.tsx
+++ app/ui/savings/goals-form.tsx
@@ -3,7 +3,6 @@
 import Link from "next/link";
 import {
   CurrencyDollarIcon,
-  UserCircleIcon,
   ArrowRightIcon
 } from "@heroicons/react/24/outline";
 import { useActionState } from "react";
